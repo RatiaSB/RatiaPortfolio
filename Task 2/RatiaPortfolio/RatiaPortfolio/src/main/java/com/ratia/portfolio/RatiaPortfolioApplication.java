f8a849460236b96377deea51df0ccf9febbb88c5
RatiaSB <230724477@mycput.ac.za>
2026-01-14 10:02:01 +0200

    Task 2 started, I develop REST API my project management system

---
+++ Task 2/RatiaPortfolio/RatiaPortfolio/src/main/java/com/ratia/portfolio/RatiaPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.ratia.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatiaPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatiaPortfolioApplication.class, args);
+	}
+
+}
---
+++ Task 2/RatiaPortfolio/RatiaPortfolio/src/test/java/com/ratia/portfolio/RatiaPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratia.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatiaPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
